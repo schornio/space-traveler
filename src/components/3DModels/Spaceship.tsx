eeb6d06623e8594460442e62844c604d5edaec93
SoftEngMatthew <mateus.fonto@gmail.com>
2024-02-26 14:32:15 +0100

    refactor: added useSpaceship hook

--- src/components/3DModels/Spaceship.tsx
+++ src/components/3DModels/Spaceship.tsx
@@ -14,11 +14,8 @@ import { GLTF } from "three-stdlib";
 import { toRotation } from "../../utils/toRotation";
 import { toPosition } from "../../utils/toPosition";
 import { PATH_3D_MODELS } from "./path";
-import { schornColors } from "../../constants/schornColors";
-import { KeyboardEvent, useEffect, useRef, useState } from "react";
-import { useFrame } from "@react-three/fiber";
-import { Laser, LaserProps } from "./Laser";
-import { fromPixelsToMeters } from "../../utils/fromPixelsToMeters";
+import { Laser } from "./Laser";
+import { useSpaceship } from "../../hooks/useSpaceship";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -39,118 +36,7 @@ export function Spaceship() {
   const { nodes, materials } = useGLTF(
     `${PATH_3D_MODELS}/spaceshipOptimized-transformed.glb`
   ) as GLTFResult;
-  const ref = useRef<THREE.Group | null>(null);
-  const [movement, setMovement] = useState({
-    up: false,
-    down: false,
-    left: false,
-    right: false,
-  });
-  const speed = 1;
-  const [lasers, setLasers] = useState<LaserProps[]>([]);
-
-  useEffect(() => {
-    const handleKeyDown = (e) => {
-      console.log("I was pressed");
-
-      switch (e.key) {
-        case "w":
-          setMovement((m) => ({ ...m, up: true }));
-          break;
-        case "s":
-          setMovement((m) => ({ ...m, down: true }));
-          break;
-        case "a":
-          setMovement((m) => ({ ...m, left: true }));
-          break;
-        case "d":
-          setMovement((m) => ({ ...m, right: true }));
-          break;
-      }
-    };
-
-    const handleKeyUp = (e) => {
-      switch (e.key) {
-        case "w":
-          setMovement((m) => ({ ...m, up: false }));
-          break;
-        case "s":
-          setMovement((m) => ({ ...m, down: false }));
-          break;
-        case "a":
-          setMovement((m) => ({ ...m, left: false }));
-          break;
-        case "d":
-          setMovement((m) => ({ ...m, right: false }));
-          break;
-      }
-    };
-
-    window.addEventListener("keydown", handleKeyDown);
-    window.addEventListener("keyup", handleKeyUp);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeyDown);
-      window.removeEventListener("keyup", handleKeyUp);
-    };
-  }, []);
-
-  useEffect(() => {
-    const fireLaser = (e) => {
-      if (e.code === "Space" && ref.current) {
-        const currentPosition = ref.current.position;
-        ref.current.getWorldPosition(currentPosition);
-
-        setLasers((lasers) => [
-          ...lasers,
-          {
-            id: `laser-${lasers.length}`,
-            position: currentPosition,
-          },
-        ]);
-      }
-    };
-
-    window.addEventListener("keydown", fireLaser);
-
-    return () => window.removeEventListener("keydown", fireLaser);
-  }, []);
-
-  useFrame(() => {
-    if (ref.current) {
-      const delta = speed / 10;
-      let newPositionY = ref.current.position.y;
-      let newPositionX = ref.current.position.x;
-
-      if (movement.up) {
-        newPositionY += delta;
-      }
-      if (movement.down) {
-        newPositionY -= delta;
-      }
-      if (movement.left) {
-        newPositionX -= delta;
-      }
-      if (movement.right) {
-        newPositionX += delta;
-      }
-
-      const windowHalfX = fromPixelsToMeters(window.innerWidth / 2);
-      const windowHalfY = fromPixelsToMeters(window.innerHeight / 2);
-
-      newPositionX = Math.max(
-        -windowHalfX,
-        Math.min(windowHalfX, newPositionX)
-      );
-      newPositionY = Math.max(
-        -windowHalfY,
-        Math.min(windowHalfY, newPositionY)
-      );
-
-      ref.current.position.y = newPositionY;
-      ref.current.position.x = newPositionX;
-    }
-  });
+  const { ref, lasers } = useSpaceship();
 
   return (
     <group>
