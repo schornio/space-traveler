21266b9cd9f8706433b3575b7f7024d275a3f3e9
SoftEngMatthew <mateus.fonto@gmail.com>
2024-02-28 14:02:09 +0100

    feat: added galaxy background + Created component HandVRControls

---
+++ src/components/HandVRControls/HandVRControls.tsx
@@ -0,0 +1,95 @@
+import { useCallback } from "react";
+import { toPosition } from "../../utils/toPosition";
+import { ActionControls } from "../../store/useControlsStore";
+import { toRotation } from "../../utils/toRotation";
+import Button from "../Button";
+
+type HandVRControlsProps = {
+  setControls: (action: keyof ActionControls, value: boolean) => void;
+};
+
+export function HandVRControls({ setControls }: HandVRControlsProps) {
+  const onVRHandInteraction = useCallback(
+    (action: keyof ActionControls, value: boolean) => {
+      setControls(action, value);
+    },
+    []
+  );
+
+  return (
+    <group
+      position={toPosition({
+        positionLeft: 0.3,
+        positionTop: 0.2,
+        positionIn: 0.2,
+      })}
+      scale={0.8}
+      rotation={toRotation({
+        rotationXInDeg: -20,
+      })}
+    >
+      <Button
+        positionTop={1}
+        onPressStart={() => {
+          onVRHandInteraction("up", true);
+        }}
+        onPressEnd={() => {
+          onVRHandInteraction("up", false);
+        }}
+      >
+        👆
+      </Button>
+
+      <Button
+        positionTop={0.6}
+        onPressStart={() => {
+          onVRHandInteraction("down", true);
+        }}
+        onPressEnd={() => {
+          onVRHandInteraction("down", false);
+        }}
+      >
+        👇
+      </Button>
+
+      <Button
+        positionTop={0.8}
+        positionLeft={0.25}
+        onPressStart={() => {
+          onVRHandInteraction("left", true);
+        }}
+        onPressEnd={() => {
+          onVRHandInteraction("left", false);
+        }}
+      >
+        👈
+      </Button>
+
+      <Button
+        positionTop={0.8}
+        positionRight={0.25}
+        onPressStart={() => {
+          onVRHandInteraction("right", true);
+        }}
+        onPressEnd={() => {
+          onVRHandInteraction("right", false);
+        }}
+      >
+        👉
+      </Button>
+
+      <Button
+        positionTop={0.8}
+        positionRight={1}
+        onPressStart={() => {
+          onVRHandInteraction("shoot", true);
+        }}
+        onPressEnd={() => {
+          onVRHandInteraction("shoot", false);
+        }}
+      >
+        🔫
+      </Button>
+    </group>
+  );
+}
---
+++ src/components/SphereBackground/SphereBackground.tsx
@@ -0,0 +1,39 @@
+import { useLoader } from "@react-three/fiber";
+import { BackSide, TextureLoader } from "three";
+import galaxyBg from "/galaxy_2.jpg";
+import { RotationRad, toRotation } from "../../utils/toRotation";
+
+export function SphereBackground() {
+  const texture = useLoader(TextureLoader, galaxyBg);
+
+  return (
+    <group>
+      <mesh
+        scale={[-1, 1, 1]}
+        rotation={toRotation({
+          rotationYInRad: -RotationRad.QUARTER,
+        })}
+      >
+        {/* original radius 500 */}
+        <sphereGeometry args={[100, 120, 80]} />
+        <meshBasicMaterial side={BackSide} map={texture} />
+      </mesh>
+
+      {/* <SphereOverlay /> */}
+    </group>
+  );
+}
+
+function SphereOverlay({ opacity = 0.5 }: { opacity?: number }) {
+  return (
+    <mesh>
+      <sphereGeometry args={[9, 32, 32]} />
+      <meshBasicMaterial
+        color="black"
+        side={BackSide}
+        opacity={opacity}
+        transparent
+      />
+    </mesh>
+  );
+}
