6171a947573a2d700dfbf5ed6572c95446a9a2b7
SoftEngMatthew <mateus.fonto@gmail.com>
2024-02-28 13:00:56 +0100

    feat: improved design canvas web
    It far from the final version, but going in the right direction

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,89 @@
+import {
+  ImmersiveSessionOrigin,
+  useEnterXR,
+} from "@coconut-xr/natuerlich/react";
 import { CoreGame } from "./CoreGame";
 import { CellphoneControls } from "./components/CellphoneControls";
 import "./globals.css";
 import { useCurrentDevice } from "./store/useCurrentDevice";
+import { Controllers, Hands, XRCanvas } from "@coconut-xr/natuerlich/defaults";
+import { Canvas } from "@react-three/fiber";
+import { useGameStore } from "./store/useGameStore";
+
+const sessionOptions: XRSessionInit = {
+  requiredFeatures: ["local-floor", "hand-tracking"],
+};
 
 function App() {
   const currentDevice = useCurrentDevice();
+  const enterVR = useEnterXR("immersive-ar", sessionOptions);
+  const { healthSpaceship, score } = useGameStore((state) => ({
+    healthSpaceship: state.healthSpaceship,
+    score: state.score,
+  }));
 
   return (
-    <>
-      <div className="cellphone-container">
-        <h1
+    <main>
+      <div
+        style={{
+          position: "absolute",
+          top: "10%",
+          height: "10%",
+          width: "100%",
+          color: "white",
+          display: "flex",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+          fontSize: currentDevice === "cellphone" ? "1rem" : "2rem",
+        }}
+      >
+        <p>{`Health: ${String(healthSpaceship).toUpperCase()}`}</p>
+        <p>{currentDevice.toUpperCase()}</p>
+        <p>{`Score: ${String(score).toUpperCase()}`}</p>
+      </div>
+
+      {currentDevice === "cellphone" && (
+        <div
           style={{
             position: "absolute",
-            top: "10px",
-            left: "50%",
+            bottom: "10%",
+            height: "10%",
+            width: "100%",
             color: "white",
-            transform: "translateX(-50%)",
+            display: "flex",
+            justifyContent: "space-evenly",
+            alignItems: "center",
+            fontSize: currentDevice === "cellphone" ? "1rem" : "2rem",
           }}
         >
-          {currentDevice.toUpperCase()}
-        </h1>
+          <CellphoneControls />
+        </div>
+      )}
 
-        <CellphoneControls />
-      </div>
-
+      {/* Web canvas */}
       <div className="canvas-container">
-        <CoreGame />
+        <Canvas>
+          <CoreGame />
+        </Canvas>
       </div>
-    </>
+
+      {currentDevice === "vr" && (
+        <button onClick={enterVR} className="enter-vr-btn">
+          Enter VR
+        </button>
+      )}
+
+      <div>
+        <XRCanvas>
+          <ImmersiveSessionOrigin>
+            <Hands type="grab" />
+            <Controllers type="pointer" />
+
+            <CoreGame />
+          </ImmersiveSessionOrigin>
+        </XRCanvas>
+      </div>
+    </main>
   );
 }
 
--- src/CoreGame.tsx
+++ src/CoreGame.tsx
@@ -1,12 +1,8 @@
-import { Center, OrbitControls, Text, Text3D } from "@react-three/drei";
-import { toPosition } from "./utils/toPosition";
-import { fontSize } from "./utils/fontSizes";
+import { OrbitControls } from "@react-three/drei";
 import { useGameStore } from "./store/useGameStore";
 import { Floor } from "./components/Floor";
 import { memo, useEffect } from "react";
-import { schornColors } from "./constants/schornColors";
 import { Spaceship } from "./components/3DModels/Spaceship";
-import { Canvas } from "@react-three/fiber";
 import "./globals.css";
 import useControlsStore from "./store/useControlsStore";
 
@@ -14,12 +10,8 @@ const FloorMemoized = memo(Floor);
 const COLLISION_TIME_INTERVAL = 500;
 
 export function CoreGame() {
-  const { healthSpaceship, score, isShipHitByAsteroid } = useGameStore(
-    (state) => ({
-      isShipHitByAsteroid: state.isShipHitByAsteroid,
-      healthSpaceship: state.healthSpaceship,
-      score: state.score,
-    })
+  const isShipHitByAsteroid = useGameStore(
+    (state) => state.isShipHitByAsteroid
   );
   const initializeKeyboard = useControlsStore(
     (state) => state.initializeKeyboard
@@ -38,43 +30,13 @@ export function CoreGame() {
   }, [isShipHitByAsteroid]);
 
   return (
-    <Canvas>
-      <Center
-        position={toPosition({
-          positionTop: 2,
-          positionLeft: 3,
-        })}
-      >
-        <Text3D
-          font="/neo_cybern.json"
-          size={fontSize.xl}
-          curveSegments={24}
-          scale={[1, 1, 0.2]}
-        >
-          Health: {healthSpaceship}
-          <meshStandardMaterial
-            attach="material"
-            color={schornColors.magenta}
-          />
-        </Text3D>
-      </Center>
-
-      <Text
-        fontSize={fontSize.xl}
-        color={schornColors.magenta}
-        position={toPosition({
-          positionTop: 2,
-          positionRight: 3,
-        })}
-      >
-        Score: {score}
-      </Text>
+    <group>
       <ambientLight intensity={9} />
 
       <OrbitControls />
 
       <Spaceship />
       <FloorMemoized />
-    </Canvas>
+    </group>
   );
 }
